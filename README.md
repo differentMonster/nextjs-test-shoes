654db662e60d3789b339c94cb33f86575d5db078
differentMonster <differentcodemonster@gmail.com>
2022-07-04 18:24:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-gxkpyg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-gxkpyg)
---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { Suspense, useRef, useState, useEffect } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { ContactShadows, Environment, useGLTF, OrbitControls } from "@react-three/drei"
+
+function Shoe() {
+  const ref = useRef()
+  const snap = useSnapshot(state)
+  // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
+  // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
+  // nodes is a named collection of meshes, materials a named collection of materials
+  const { nodes, materials } = useGLTF("shoe-draco.glb")
+
+  // Animate model
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    ref.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20
+    ref.current.rotation.x = Math.cos(t / 4) / 8
+    ref.current.rotation.y = Math.sin(t / 4) / 8
+    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+  })
+
+  // Using the GLTFJSX output here to wire in app-state and hook up events
+  return (
+    <group
+      ref={ref}
+      dispose={null}>
+      <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+    </group>
+  )
+}
+
+
+export default function Application() {
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <Suspense fallback={null}>
+          <Shoe />
+          <Environment preset="city" />
+          <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
+        </Suspense>
+        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+      </Canvas>
+    </>
+  )
+}
